32608444bcd388c9835dd116a861e7632e17a42b
perez <9182@htl.rennweg.at>
2022-02-20 13:36:24 +0100

    Added coin ui

--- scenes/BaseLevel.gd
+++ scenes/BaseLevel.gd
@@ -1,12 +1,28 @@
 extends Node
 
+signal coin_total_changed
+
 var playerScene = preload("res://scenes/Player.tscn")
 var spawnPosition = Vector2.ZERO
 var currentPlayerNode = null
+var totalCoins = 0
+var collectedCoins = 0
 
 func _ready():
 	spawnPosition = $Player.global_position
 	register_player($Player)
+	
+	coin_total_changed(get_tree().get_nodes_in_group("coin").size())
+	
+	
+func coin_collected():
+	collectedCoins += 1
+	emit_signal("coin_total_changed", totalCoins, collectedCoins)
+	
+	
+func coin_total_changed(newTotal):
+	totalCoins = newTotal
+	emit_signal("coin_total_changed", totalCoins, collectedCoins)
 	
 	
 func register_player(player):
@@ -15,12 +31,14 @@ func register_player(player):
 	# CONNECT_DEFERRED -> flag to avoid errors
 	currentPlayerNode.connect("died", self, "on_player_died", [], CONNECT_DEFERRED)
 	
+	
 func create_player():
 	var playerInstance = playerScene.instance()
 	# keep node position in node tree
 	add_child_below_node(currentPlayerNode, playerInstance)
 	playerInstance.global_position = spawnPosition
 	register_player(playerInstance)
+	
 	
 func on_player_died():
 	currentPlayerNode.queue_free()
--- scenes/Coin.gd
+++ scenes/Coin.gd
@@ -7,6 +7,9 @@ func on_area_entered(area2d):
 	# on next idle frame -> remove node
 	$AnimationPlayer.play("pickup")
 	call_deferred("disable_pickup")
+	
+	var baseLevel = get_tree().get_nodes_in_group("base_level")[0]
+	baseLevel.coin_collected()
 
 func disable_pickup():
 	$Area2D/CollisionShape2D.disabled = true
