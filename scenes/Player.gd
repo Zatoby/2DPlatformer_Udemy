250e794dc172750bf1fb357bfe71741b54139a5c
perez <9182@htl.rennweg.at>
2022-02-20 11:43:22 +0100

    Added dash, level_001, flag, new notes

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -2,19 +2,50 @@ extends KinematicBody2D
 
 signal died
 
+enum State { NORMAL, DASHING }
+
+export(int, LAYERS_2D_PHYSICS) var dashHazardMask
+
 var gravity = 1000
 var velocity = Vector2.ZERO
 var maxHorizontalSpeed = 140
+var maxDashSpeed = 500
+var minDashSpeed = 200
 var horizontalAcceleration = 2000
-var jumpSpeed = 360
+var jumpSpeed = 320
 var jumpTerminationMultiplier = 4
 var hasDoubleJump = false
+var hasDash = false
+var currentState = State.NORMAL
+var isStateNew = true
+
+var defaultHazardMask = 0
+
 
 func _ready():
 	$HazardArea.connect("area_entered", self, "on_hazard_area_entered")
+	defaultHazardMask = $HazardArea.collision_mask
 	
 	# delta -> number of seconds passed since last frame
 func _process(delta):
+	match currentState:
+		State.NORMAL:
+			process_normal(delta)
+		State.DASHING:
+			process_dash(delta)
+	
+	isStateNew = false
+	
+func change_state(newState):
+	currentState = newState
+	isStateNew = true
+	
+# handle regular movement
+func process_normal(delta):
+	if (isStateNew):
+		$DashArea/CollisionShape2D.disabled = true
+		$HazardArea.collision_mask = defaultHazardMask
+		
 	var moveVector = get_movement_vector()
 	
 	velocity.x += moveVector.x * horizontalAcceleration * delta
@@ -47,8 +78,35 @@ func _process(delta):
 
 	if (is_on_floor()):
 		hasDoubleJump = true
-
+		hasDash = true
+		
+	if (hasDash && Input.is_action_just_pressed("dash")):
+		call_deferred("change_state", State.DASHING)
+		hasDash = false
+		
 	update_animation()
+
+
+func process_dash(delta):
+	if (isStateNew):
+		$DashArea/CollisionShape2D.disabled = false
+		$AnimatedSprite.play("jump")
+		$HazardArea.collision_mask = dashHazardMask
+		var moveVector = get_movement_vector()
+		var velocityMod = 1
+		if (moveVector.x != 0):
+			# sign -> if value negative -1 if positive 1
+			velocityMod = sign(moveVector.x)
+		else:
+			velocityMod = 1 if $AnimatedSprite.flip_h else -1
+		
+		velocity = Vector2(maxDashSpeed * velocityMod, 0)
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
+	velocity.x = lerp(0, velocity.x, pow(2, -8 * delta))
+	
+	if (abs(velocity.x) < minDashSpeed):
+		call_deferred("change_state", State.NORMAL)
 
 func get_movement_vector():
 	var moveVector = Vector2.ZERO
@@ -57,6 +115,7 @@ func get_movement_vector():
 	# checks action only once stops checking after press -> no hold jump-repeats
 	moveVector.y = -1 if Input.is_action_just_pressed("jump") else 0
 	return moveVector
+	
 	
 # animate sprite
 func update_animation():
@@ -71,6 +130,7 @@ func update_animation():
 		
 	if (moveVec.x != 0):
 		$AnimatedSprite.flip_h = true if moveVec.x > 0 else false
+
 
 func on_hazard_area_entered(area2d):
 	emit_signal("died")
